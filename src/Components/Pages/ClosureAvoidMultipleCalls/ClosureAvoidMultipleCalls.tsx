5586e6c5b477f3513cfafbdb0da2574adbdf8681
XoseAbel <lopezlopezjoseabel@gmail.com>
2020-11-23 09:28:42 +0100

    case issue

--- src/Components/Pages/ClosureAvoidMultipleCalls/ClosureAvoidMultipleCalls.tsx
+++ src/Components/Pages/ClosureAvoidMultipleCalls/ClosureAvoidMultipleCalls.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default () => {
+export const ClosureAvoidMultipleCalls = () => {
   return <p>hola</p>;
 };
---
+++ src/Components/Pages/ClosureAvoidMultipleCalls/index.ts
@@ -0,0 +1,1 @@
+export { ClosureAvoidMultipleCalls as default } from './ClosureAvoidMultipleCalls';
